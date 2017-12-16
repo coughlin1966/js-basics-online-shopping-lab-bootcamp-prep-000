8facd203818cd0749fd3714a01bedd35e0e75cdb
Peter Coughlin <coughlin1966@gmail.com>
2017-12-16 20:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ getCart()
   if (cardNumber == 0) {
     console.log('Sorry, we don\'t have a credit card on file for you.');
   }
-return false
+
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   }
