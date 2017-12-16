204e3a25d2b6ac741b644f84a3952528b43c6922
Peter Coughlin <coughlin1966@gmail.com>
2017-12-16 19:56:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function placeOrder(cardNumber) {
 getCart()
 
   if (cardNumber == 0) {
-    console.log('Sorry, we don\'t have a credit card on file for you.')
+    console.log('Sorry, we don\'t have a credit card on file for you.');
   return false}
    else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
