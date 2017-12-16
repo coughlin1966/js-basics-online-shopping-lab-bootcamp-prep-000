f7a5c86bfe82a3d96ba45cb3031703803f2c3c14
Peter Coughlin <coughlin1966@gmail.com>
2017-12-16 19:37:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ for (let i = 0; i < cart.length; i++) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber == 0) {
+  if (!cardNumber) {
     console.log('Sorry, we don\'t have a credit card on file for you.')
   } else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
